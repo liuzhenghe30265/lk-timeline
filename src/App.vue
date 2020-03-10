08a0fd3bafd68944203873911e063249fcc23e25
liuzhenghe30265 <15901450207@163.com>
2020-03-10 20:23:09 +0800

    feat:init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Main :msg="msg"></Main>
+</template>
+
+<script>
+import Main from './components/Main'
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg: 'time-line'
+    }
+  },
+  components: {
+    Main
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{this.msg}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'lk-timeline',
+  data() {
+    return {}
+  },
+  props: {
+    msg: {
+      type: String,
+      default: 'default'
+    }
+  }
+}
+</script>
